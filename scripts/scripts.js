aa25f98dfb3850d2425e9de79b38229229dca5c7
Luisa Oliveira <luisagarrozi@gmail.com>
2021-07-13 23:08:29 -0300

    Primeiro Commit

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+// Função do email
+(() => {
+    const button = document.getElementById('enviar');
+    const input = document.getElementById('email-input');
+
+    button.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        localStorage.setItem('email', input.value);
+    })
+})();
